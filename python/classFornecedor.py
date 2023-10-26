25cc93048130743dedb349b97d3716988f765f47
Vinícius <vinileardini@gmail.com>
2023-10-26 13:51:26 -0300

    desenvolvimento fornecedores

--- python/classFornecedor.py
+++ python/classFornecedor.py
@@ -1,4 +1,6 @@
-from typing import Any
+import json
+import tempfile
+import shutil
 
 
 class Fornecedor:
@@ -10,35 +12,71 @@ class Fornecedor:
         self.email = email
         self.produtos = produtos
         
-    
-    def getNome(self):
+    #Feito
+    def cadastrarFornecedor(self):
         
-        return self.nomeFornecedor
-    
-    def getEmailFornecedor(self):
+        dadosFornecedor = {'telefone':self.telefoneFornecedor, 'email':self.email, 'produtos':self.produtos}
         
-        return self.email
-    
-    def getTelefoneFornecedor(self):
+        with open('arquivos\cadastroFornecedor.json','r') as arquivoFornecedores,\
+            tempfile.NamedTemporaryFile('w',delete=False) as tempFornecedores:
+            
+            fornecedores = json.load(arquivoFornecedores)
+            
+            if not fornecedores:
+                #verifica se já existe fornecedor com esse nome
+                if self.nomeFornecedor not in fornecedores:
+                    novoFornecedor = {}
+                    novoFornecedor[self.nomeFornecedor] = dadosFornecedor
+                    json.dump(novoFornecedor,tempFornecedores,ensure_ascii=False,indent=4)
+                else:
+                    print('Fornecedor já existente')
+                
+            else:
+                fornecedores[self.nomeFornecedor] = dadosFornecedor
+                json.dump(fornecedores,tempFornecedores,ensure_ascii=False,indent=4)
         
-        return self.telefoneFornecedor
-    
-    def getProdutosFornecedor(self):
+        shutil.move(tempFornecedores.name,'arquivos\cadastroFornecedor.json')
+                
+                
+                
+                
+#Feito
+def getNome(nome):
         
-        for produto in self.produtos:
-            print(self.produtos[produto])
-    
-    def setNome(self,alteraNome):
+        with open('arquivos\cadastroFornecedor.json','r') as arquivo:
         
-        self.nomeFornecedor = alteraNome
-    
-    def setEmailFornecedor(self,novoEmail):
+            conteudoArquivo = json.load(arquivo)
         
-        self.email = novoEmail
+            if nome in conteudoArquivo:
+                print(f'O fornecedor {nome} foi encontrado')
+                print('Email para contato:',conteudoArquivo[nome]["email"])
+                print('Telefone:',conteudoArquivo[nome]["telefone"])
+                print('Produtos oferecidos:',conteudoArquivo[nome]["produtos"])
+            
+            else:
+                print('Não existe fornecedor com esse nome')
     
-    def setTelefoneFornecedor(self,novoTelefone):
-        
-        self.telefoneFornecedor = novoTelefone
+
+
+
+def setEmailFornecedor(nome,novoEmail):
+    
+    with open('arquivos\cadastroFornecedor.json','r') as arquivo,\
+        tempfile.NamedTemporaryFile('w',delete=False) as alteracaoEmail:
+            
+        conteudoArquivo = json.load(arquivo)
+    
+        if nome in conteudoArquivo:
+            conteudoArquivo[nome]["email"] = novoEmail
+    
+        else:
+            print('Não existe fornecedor com esse nome')   
+            
+    shutil.move(alteracaoEmail.name,'arquivos\cadastroFornecedor.json') 
+
+def setTelefoneFornecedor(self,novoTelefone):
+    
+    self.telefoneFornecedor = novoTelefone
 
     
         
--- python/main.py
+++ python/main.py
@@ -2,10 +2,25 @@ from classFornecedor import Fornecedor
 from classProduto import Produto
 from classPedido import Pedido
 import classPedido
+import classFornecedor as fornecedores
 
 
 f1 = Fornecedor('a',123,'abc@gmail.com',['asd','dsads'])
+f1.cadastrarFornecedor()
 
+f2 = Fornecedor('b',456,'def@gmail.com',['oi','tchau'])
+f2.cadastrarFornecedor()
+
+
+fornecedores.getNome('b')
+
+#fornecedores.setEmailFornecedor('b','12345@gmail.com')
+
+fornecedores.getNome('b')
+
+#fornecedores.getNome('c')
+
+'''
 p1 = Pedido('12874',['dsa','oi'])
 
 p1.realizarPedido()
@@ -14,6 +29,7 @@ p2 = Pedido('678',[1,2])
 
 p2.realizarPedido()
 
-#classPedido.pesquisaPedido("12874")
+classPedido.pesquisaPedido("12874")
 
-#classPedido.excluiPedido("678")
+classPedido.excluiPedido("678")
+'''
