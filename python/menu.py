ebbebe3ba90f7173c417732b265c6d1d4111e062
Vinícius <vinileardini@gmail.com>
2023-11-02 22:59:43 -0300

    criacao pedido atraves do menu

--- python/menu.py
+++ python/menu.py
@@ -1,4 +1,6 @@
 import json
+import classPedido
+from classPedido import Pedido
 
 def menu():
     
@@ -11,7 +13,7 @@ def menu():
         escolha = input('Selecione a opção desejada:')
         print('*************************************************')
         
-        if escolha == '1':
+        if escolha == "1":
             
             inputUsuario = input('Usuario:')
             senhaUsuario = input('Senha:')
@@ -24,6 +26,90 @@ def menu():
                     if infoUsuarios[inputUsuario]["senha"] == senhaUsuario:
                         print('*************************************************')
                         print('Logado com sucesso...')
+                        print('*************************************************')
+                        print('Menu funcionário')
+                        print('*************************************************')
+                        print('1 - Verificar estoque')
+                        print('2 - Realizar pedido')
+                        print('3 - Finalizar pedido')
+                        print('4 - Verificar pedidos')
+                        print('5 - Cadastrar produto')
+                        print('6 - Cadastrar fornecedor')
+                        print('7 - Verificar movimentações')
+                        print('0 - Sair')
+                        escolhaUsuario = input('Selecione a opção desejada:')
+                        print('*************************************************')
+                        
+                        
+                        #if escolhaUsuario == "1":
+                            
+                            
+                        
+                        if escolhaUsuario == "2":
+                            
+                            inputNumero = input('Informe o número do pedido:')
+                            inputTipo = input('Insira o tipo de pedido (entrada/saida):')
+                            itens = []
+                            
+                            while True:
+                                inputItem = input('Insira o item no pedido:')
+                                itens.append(inputItem)
+                                
+                                print('*************************************************')
+                                
+                                print('0 - Parar inserção de itens no pedido')
+                                print('1 - Adicionar novo item')
+                                
+                                continueItem = input('Informe sua escolha:')
+                                
+                                if continueItem == "0":
+                                    break
+                                
+                                print('*************************************************')
+
+
+                                
+                            print('*************************************************')
+                            
+                            novoPedido = Pedido(inputNumero,inputTipo,itens)
+                            
+                            print('Pedido criado')
+                            novoPedido.getInfo()
+                        
+                            
+                        '''    
+                        
+                        elif escolhaUsuario == "3":
+                            
+                            
+                        
+                        elif escolhaUsuario == "4":
+                            
+                        
+                        
+                        elif escolhaUsuario == "5":
+                            
+                        
+                        elif escolhaUsuario == "6":
+                            
+                        
+                        elif escolhaUsuario == "7":
+                            
+                        
+                        
+                        elif escolhaUsuario == "0":
+                        
+                        
+                        else:
+                            print("Valor inválido")
+                            
+                        
+                        
+                    
+
+                        
+                        
+                        
                     
                 else:
                     print('************************** ***********************')
@@ -50,5 +136,7 @@ def menu():
         elif escolha == '0':
             print('Encerrando...')
             return False
+        '''
             
+    
             
